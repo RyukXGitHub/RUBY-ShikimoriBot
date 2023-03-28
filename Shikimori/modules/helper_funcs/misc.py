943a62805a5b87ed118e9abe9cc551f536ff2e32
RyukXGitHub <129211074+RyukXGitHub@users.noreply.github.com>
2023-03-29 02:36:13 +0530

    Update misc.py

--- Shikimori/modules/helper_funcs/misc.py
+++ Shikimori/modules/helper_funcs/misc.py
@@ -63,7 +63,7 @@ def split_message(msg: str) -> List[str]:
     return result
 
 
-# def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
+# def paginate_modules(paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
 #     if not chat:
 #         modules = sorted(
 #             [
@@ -130,30 +130,20 @@ def paginate_modules(page_n, module_dict, prefix, chat=None):
             ]
         )
 
-    pairs = list(zip(modules[::3], modules[1::3], modules[2::3]))
-    i = 0
-    for m in pairs:
-        for _ in m:
-            i += 1
-    if len(modules) - i == 1:
-        pairs.append((modules[-1],))
-    elif len(modules) - i == 2:
-        pairs.append(
-            (
-                modules[-2],
-                modules[-1],
-            )
-        )
+    pairs = [modules[i * 3 : (i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)]
+    round_num = len(modules) / 3
+    calc = len(modules) - round(round_num)
+    if calc in [1, 2]:
+    pairs.append((modules[-1],))
+    
 
-    COLUMN_SIZE = 8
-
-    max_num_pages = ceil(len(pairs) / COLUMN_SIZE)
+    max_num_pages = ceil(len(pairs) / 6)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
-    if len(pairs) > COLUMN_SIZE:
+    if len(pairs) > 3:
         pairs = pairs[
-            modulo_page * COLUMN_SIZE : COLUMN_SIZE * (modulo_page + 1)
+            modulo_page * 6 : 6 * (modulo_page + 1)
         ] + [
             (
                 EqInlineKeyboardButton("❮", callback_data="{}_prev({})".format(prefix, modulo_page),),
