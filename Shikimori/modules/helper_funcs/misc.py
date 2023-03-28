7dec8eb6d8e9a99d62cf6bb4f4e7a54c747d0da6
RyukXGitHub <129211074+RyukXGitHub@users.noreply.github.com>
2023-03-29 02:51:30 +0530

    Update misc.py

--- Shikimori/modules/helper_funcs/misc.py
+++ Shikimori/modules/helper_funcs/misc.py
@@ -71,7 +71,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     round_num = len(modules) / 3
     calc = len(modules) - round(round_num)
     if calc in [1, 2]:
-    pairs.append((modules[-1],))
+        pairs.append((modules[-1],))
     
 
     max_num_pages = ceil(len(pairs) / 6)
